1c0ade6bc513e31529bc5e55a307ad4de449a353
nnarct <nannapat1551@gmail.com>
2023-11-19 00:51:05 +0700

    feat: player get dark when active pause

--- Assets/Scripts/Pausegame/PauseMenu.cs
+++ Assets/Scripts/Pausegame/PauseMenu.cs
@@ -5,13 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
+    public GameObject player;
     public GameObject pauseMenu;
     public bool isPaused;
-
     private Interactor interactorScript;
+    private SpriteRenderer playerSpriteRenderer;
 
     void Start()
     {
+        playerSpriteRenderer = player.GetComponent<SpriteRenderer>();
         pauseMenu.SetActive(false);
         interactorScript = GameObject.FindWithTag("Interactable").GetComponent<Interactor>();
     }
@@ -42,12 +44,14 @@ public class PauseMenu : MonoBehaviour
     public void PauseGame()
     {
         pauseMenu.SetActive(true);
+        playerSpriteRenderer.color = new Color(0.3608f, 0.3608f, 0.3608f);
         Time.timeScale = 0f;
         isPaused = true;
     }
 
     public void ResumeGame()
     {
+        playerSpriteRenderer.color = new Color(1f, 1f, 1f);
         pauseMenu.SetActive(false);
         Time.timeScale = 1f;
         isPaused = false;

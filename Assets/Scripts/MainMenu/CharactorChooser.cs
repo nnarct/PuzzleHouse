b8e4ac5d4c686c4abd3969aa7617dfe2500832a4
wishcream <wishcream6@gmail.com>
2023-11-23 11:37:52 +0700

    add: character chooser method

--- Assets/Scripts/MainMenu/CharactorChooser.cs
+++ Assets/Scripts/MainMenu/CharactorChooser.cs
@@ -2,13 +2,14 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class CharacterChooser : MonoBehaviour
-{
+{ 
     //string[] Charactor = { "Alan" ,"Atanasoff", "Edward", "Humphrey"};
-    public void ChooseCharactorByName (string name)
+    public void ChooseCharacterByName (string name)
     {
-        PlayerPrefs.SetString("charactor", name);
+        PlayerPrefs.SetString("character", name);
         PlayerPrefs.Save();
         SceneManager.LoadScene("EnterPlayerName");
     }
-    
+
+    //string choosenCharacter = PlayerPrefs.GetString("character");
 }
--- Assets/Scripts/SkinLoader.cs
+++ Assets/Scripts/SkinLoader.cs
@@ -1,13 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SkinLoader : MonoBehaviour
 {
-    public GameObject skinToLoad;
+    public GameObject alanPrefab; // Assign prefabs in the Inspector for different characters
+    public GameObject jennyPrefab;
+    public GameObject humphreyPrefab;
 
     private void Awake()
     {
-        Instantiate(skinToLoad, transform);
+        string choosenCharacter = PlayerPrefs.GetString("character");
+
+        switch (choosenCharacter)
+        {
+            case "Alan":
+                Instantiate(alanPrefab, transform);
+                break;
+            case "Jenny":
+                Instantiate(jennyPrefab, transform);
+                break;
+            case "Humphrey":
+                Instantiate(humphreyPrefab, transform);
+                break;
+            default:
+                Debug.LogWarning("No character selected or character prefab not assigned.");
+                break;
+        }
     }
 }

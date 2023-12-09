490a385e96e19485464d3ddd4181b890dce25c6f
nnarct <nannapat1551@gmail.com>
2023-12-09 16:28:39 +0700

    feat: complete finish stage panel for stage1

--- Assets/Scripts/FinishStageManager.cs
+++ Assets/Scripts/FinishStageManager.cs
@@ -1,25 +1,29 @@
 using System.Collections;
-using System.Collections.Generic;
-using System.Collections.Specialized;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class FinishStageAnimation : MonoBehaviour
 {
 
     [SerializeField]
 
-    GameObject EndGamePanel, Star, UiText, NextButton, ReplayButton, Sparkling;
+    GameObject EndGamePanel, Star, UiText, NextButton, ReplayButton, LightWheel;
+
+    private Image _lightWheelImage;
     // Start is called before the first frame update
     void Awake()
     {
-        LeanTween.rotateAround(Sparkling, Vector3.forward, -360, 10f).setLoopClamp();
+        _lightWheelImage = LightWheel.GetComponent<Image>();
+        StartCoroutine(RandomColorChange());
+        LeanTween.rotateAround(LightWheel, Vector3.forward, -360, 10f).setLoopClamp();
         LeanTween.scale(Star, new Vector3(2f, 2f, 2f), 2f).setDelay(.5f).setEase(LeanTweenType.easeOutElastic).setOnComplete(ShowText);
-        LeanTween.scale(Sparkling, new Vector3(1f, 1f, 1f), 2f).setDelay(.5f).setEase(LeanTweenType.easeOutElastic);
+        LeanTween.scale(LightWheel, new Vector3(1f, 1f, 1f), 2f).setDelay(.5f).setEase(LeanTweenType.easeOutElastic);
         LeanTween.moveLocal(Star, new Vector3(0f, 240f, 0f), .7f).setDelay(2f).setEase(LeanTweenType.easeInOutCubic);
-        LeanTween.moveLocal(Sparkling, new Vector3(0f, 240f, 0f), .7f).setDelay(2f).setEase(LeanTweenType.easeInOutCubic);
+        LeanTween.moveLocal(LightWheel, new Vector3(0f, 240f, 0f), .7f).setDelay(2f).setEase(LeanTweenType.easeInOutCubic);
         LeanTween.scale(Star, new Vector3(.8f, .8f, .8f), 2f).setDelay(.5f).setEase(LeanTweenType.easeOutElastic);
-        LeanTween.scale(Sparkling, new Vector3(.7f, .7f, .7f), 2f).setDelay(.5f).setEase(LeanTweenType.easeOutElastic);
+        LeanTween.scale(LightWheel, new Vector3(.7f, .7f, .7f), 2f).setDelay(.5f).setEase(LeanTweenType.easeOutElastic);
     }
 
     void ShowText()
@@ -37,6 +41,32 @@ public class FinishStageAnimation : MonoBehaviour
         EndGamePanel.SetActive(false);
         int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
         SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    IEnumerator RandomColorChange()
+    {
+        while (true) // Infinite loop for continuous color changes
+        {
+            int randomIndex = Random.Range(0, 7);
+
+            // Define the seven possible colors
+            Color[] possibleColors =
+            {
+                new Color(1, 0, Random.value),
+                new Color(0, 1, Random.value),
+                new Color(Random.value, 0, 1),
+                new Color(Random.value, 1, 0),
+                new Color(0, Random.value, 1),
+                new Color(1, Random.value, 0),
+                Color.white // (1, 1, 1)
+            };
+
+            // Assign the color based on the random index
+            _lightWheelImage.color = possibleColors[randomIndex];
+
+            // Wait for 0.5 seconds
+            yield return new WaitForSeconds(0.5f);
+        }
     }
 
 

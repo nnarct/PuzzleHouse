2ca395ecc480427192093bb98961f44c4fd272fe
nnarct <nannapat1551@gmail.com>
2023-12-09 15:56:28 +0700

    fix: remove extraneous text

--- Assets/Scripts/FinishStageManager.cs
+++ Assets/Scripts/FinishStageManager.cs
@@ -1,26 +1,3 @@
-sddasfdfdfsad{name}sdfasdfhasdf
-Adfasdfdsfdsfdsfasdfdsfadsfdasfsdfasdfsd
-
-Thank you, Princess. It was an honor to help. I hope our paths cross again.
-
-
-PC:
-Thank you, noble[Player's Title]! Your wit and cunning have lifted the curse that bound me.
-/*The princess approaches the player, gratitude in her eyes.*/
-Your determination and intelligence have proven invaluable.I am forever grateful.
-
-PY:
-Your Highness, the honor was mine. I am grateful to have been of service. May our paths cross again in the future.
-
-PC:
-Your kindness and bravery will be remembered in the annals of our kingdom. Should you ever need aid, our doors will be open to you.
-
-PY:
-Thank you, Princess. It was an honor to help. I hope our paths cross again.
-
-PC:
-And in the future, I hope our paths cross again. Please, grace our kingdom with your presence whenever you can.
-
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;

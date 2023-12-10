05c809aea3d168a19872dfa6c0e052fa94cf33fe
wishcream <wishcream6@gmail.com>
2023-12-11 01:11:41 +0700

    edit: cloud position

--- Assets/Scripts/Outro/OutroAnimationManager.cs
+++ Assets/Scripts/Outro/OutroAnimationManager.cs
@@ -12,7 +12,7 @@ public class OutroAnimationManager : MonoBehaviour
 
     void Start()
     {
-        LeanTween.moveLocal(Cloud, new Vector3(0f, 1122f, 0f), 0f).setDelay(.5f).setEase(LeanTweenType.easeInOutQuint);
+        LeanTween.moveLocal(Cloud, new Vector3(0f, 2f, 0f), 0f).setDelay(.5f).setEase(LeanTweenType.easeInOutQuint);
         LeanTween.moveLocal(Princess, new Vector3(749f, -307f, 0f), .5f).setDelay(.5f).setEase(LeanTweenType.linear);
         LeanTween.moveLocal(Cat, new Vector3(-800f, -452f, 0f), .8f).setDelay(.5f).setEase(LeanTweenType.linear);
         MoveForthAndBack();
@@ -21,7 +21,7 @@ public class OutroAnimationManager : MonoBehaviour
     private void MoveForthAndBack()
     { 
         // Move forth
-        LeanTween.moveY(Cloud, 1100f, 2f)
+        LeanTween.moveY(Cloud, 2.5f, 2f)
             .setEase(LeanTweenType.easeInOutQuad)
             .setOnComplete(MoveBack);
     }
@@ -29,7 +29,7 @@ public class OutroAnimationManager : MonoBehaviour
     private void MoveBack()
     { 
         // Move back
-        LeanTween.moveY(Cloud, 1122f, 2f)
+        LeanTween.moveY(Cloud, 2f, 2f)
             .setEase(LeanTweenType.easeInOutQuad)
             .setOnComplete(() => MoveForthAndBack());
     }

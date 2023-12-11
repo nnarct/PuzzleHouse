25be8720e685e185efd55f66b5617f8347e7a016
wishcream <wishcream6@gmail.com>
2023-12-11 18:26:38 +0700

    style: comment code

--- Assets/Scripts/SantaPuzzle/JigsawManager.cs
+++ Assets/Scripts/SantaPuzzle/JigsawManager.cs
@@ -4,29 +4,29 @@ using UnityEngine;
 
 public class JigsawManager : MonoBehaviour
 {
-    [SerializeField] GameObject GamePanel;
-
-    public ScoreManager scoreManager;
-    
-    public string PuzzleKey = "Santa";
-
-    public List<JigsawPiece> jigsawPieces; // Drag & drop all 8 pieces in the Inspector
-
-    private List<Vector2> initialPositions;
+    public ScoreManager scoreManager; // Reference to the ScoreManager for handling puzzle completion
+    public string PuzzleKey = "Santa"; // Unique identifier for this puzzle
+    public List<JigsawPiece> jigsawPieces; // List of JigsawPiece components representing the puzzle pieces
+    [SerializeField] GameObject GamePanel; // Reference to the game panel for displaying puzzle completion
+    private List<Vector2> initialPositions; // List to store initial positions of the puzzle pieces
 
     private void Start()
     {
+        // Start a coroutine to periodically check if all puzzle pieces are in correct positions
         StartCoroutine(CheckPiecesAfterDelay());
     }
 
+    // Coroutine to check the puzzle pieces after a delay
     IEnumerator CheckPiecesAfterDelay()
     {
         bool allCorrect = false;
 
+        // Continue checking until all pieces are in correct positions
         while (!allCorrect)
         {
             allCorrect = true;
 
+            // Check each piece's correctness
             foreach (JigsawPiece piece in jigsawPieces)
             {
                 if (!piece.IsPieceCorrect())
@@ -35,19 +35,24 @@ public class JigsawManager : MonoBehaviour
                 }
             }
 
-            yield return new WaitForSeconds(1f); // Add a delay before the next check
+            // Add a delay before the next check
+            yield return new WaitForSeconds(1f);
         }
 
+        // When all pieces are in correct positions, call the Correct method
         Correct();
     }
 
+    // Add a delay before the next check
     public void Correct()
     {
         scoreManager.HandleCorrectAnswer(PuzzleKey, GamePanel);
     }
 
+    // Method to reset the positions of all puzzle pieces
     public void ResetPieces()
     {
+        // Reset each piece's position to its initial position
         for (int i = 0; i < jigsawPieces.Count; i++)
         {
             jigsawPieces[i].ResetPosition();
